bb9ada5806df11dbccb7fda78e27f0c656c58842
rafi200708 <112526729+rafi200708@users.noreply.github.com>
2022-09-02 14:40:51 +0700

    Add files via upload

---
+++ python4-tkj4.py
@@ -0,0 +1,51 @@
+#rumus persegi sisi * sisi
+
+
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("luas persegi adalah",luas_p)
+
+# rumus persegi panjang panjang * lebar
+
+panjang = 10
+lebar = 20
+luas_pp= panjang * lebar
+
+print("luas persegi panjang adalah",luas_pp)
+
+# rumus trapesium 1/2 * (a+b) * tinggi
+print ("\ntrapesium")
+a= 6
+b= 10
+t= 8
+luas_t = (a+b) * t/ 2
+print ("luas trapesium adalah", luas_t)
+
+# rumus segitiga 1/2 a * t
+
+a=5
+t=8
+luas_s = a * t /2
+print("luas segitiga  adalah",luas_s)
+
+
+# rumus jajar genjang  a * t
+
+
+a=15
+t=7
+luas_jg = a * t
+print("luas jajar genjang adalah",luas_jg)
+
+
+# luas lingkaran π * r * r * l
+
+
+phi = 3.14
+
+r =15
+r=10
+l=15
+luas_=phi * t * t * l
+print("luas lingkaran adalah", luas_s)
