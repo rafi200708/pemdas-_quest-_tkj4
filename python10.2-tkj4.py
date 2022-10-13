d179b45c53752f1aeb46949b2456d0aed7146f98
rafi200708 <112526729+rafi200708@users.noreply.github.com>
2022-10-13 08:20:42 +0700

    Add files via upload

---
+++ python10.2-tkj4.py
@@ -0,0 +1,49 @@
+# membuat Tipe data koleksi
+
+koleksi_data_str =["pisang", "mangga", "jambu", "semangka"]
+
+koleksi_data_int =[200, 300, 400, 500]
+
+koleksi_data_mix =["Rizky Billar", 100, True, "Reza Arap"]
+
+print("Koleksi data String:", koleksi_data_str)
+
+print("Koleksi data Integer:", koleksi_data_int)
+
+print("Koleksi data Campuran:", koleksi_data_mix)
+
+koleksi_nama_hewan =["jerapah", "gajah", "macan"]
+
+koleksi_nilai_uts =[80, 70, 90]
+
+koleksi_nama_teman =["untung", "isal", "ridho"]
+
+print("Koleksi nama Hewan:", koleksi_nama_hewan)
+
+print("koleksi nilai UTS:", koleksi_nilai_uts)
+
+print("koleksi nama Teman:", koleksi_nama_teman)
+
+# Buatlah 3 Kumpulan data: Nama hewan, Nilai uts,Nama teman sebangku
+
+nama_hewan =("jerapah", "anjing", "babi", "rusa")
+
+nilai_uts = [100, 200, 300, 400]
+
+nama_ts = ["udin", "jamal", "jaeng", "wiliam", "tiyan"]
+
+# tampilkan data koleksi dengan indeks
+
+print("Nama Temem Data Ke -5", nama_ts[4])
+
+# data ke 2 nama hewan
+
+print("data ke-2", nama_hewan[3])
+
+# data pertama nilai uts
+
+print("data pertama", nilai_uts[0])
+
+#data terakhir nama teman sebangku 
+
+print("data terakhir", nama_ts[-1])
